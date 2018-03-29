c066b46e3b2d82ffd51773eafe86e3c856f736f6
u1089828 <akshay.yewale@utah.edu>
2018-03-29 15:14:24 -0600

    Added Fire Spawner

---
+++ Assets/Scripts/BuildingAttachments/BuildingBulletProperties/B_SingleBullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_SingleBullet : MonoBehaviour {
+
+    bool isHoming = false;
+    float lifeTime = 1.0f;
+    float speed = 10.0f;
+
+
+    public void Initialize(float i_lifeTime = 4.0f, float i_speed = 10.0f, bool i_isHoming = false)
+    {
+        lifeTime = i_lifeTime;
+        speed = i_speed;
+        i_isHoming = isHoming;
+
+        Invoke("Destroy", lifeTime);
+    }
+
+    private void Update()
+    {
+        if(!isHoming)
+            transform.Translate(this.transform.forward.normalized * Time.deltaTime * speed);
+        else
+        {
+            Debug.LogAssertion("Homing feature for bullet is not implemented");
+        }
+    }
+
+    private void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/BuildingAttachments/FireSpawner/SingleBulletSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleBulletSpawner : MonoBehaviour {
+
+    [Header("Bullet Prefab and Properties")]
+    public GameObject bulletPrefab;
+    [SerializeField]
+    float rateOfFire = 1.0f;
+    
+
+    [SerializeField]
+    bool shouldFire = false;
+
+    private void Start()
+    {
+        if (bulletPrefab == null)
+            Debug.LogError("Bullet Prefab is not assigned");
+        
+        InvokeRepeating("Fire", 1.0f,rateOfFire);
+    }
+
+    void Fire()
+    {
+
+        if (shouldFire)
+        {
+            GameObject g0 = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            g0.GetComponent<B_SingleBullet>().Initialize();
+        }
+    }
+    
+}
