cfaa8dc6dc3aee60d6725be64c7cbcffaeb45742
u1089828 <akshay.yewale@utah.edu>
2018-04-06 22:47:07 -0600

    Merged Suraj into Akshay : Akshay

---
+++ Assets/Scripts/Buildings/Storage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storage : MonoBehaviour {
+
+    private enum StorageType
+    {
+        Fuel,
+        Metal
+    }
+
+    [SerializeField]
+    private GameObject cube;
+    public Material mat;
+    [SerializeField]
+    StorageType collectorType;
+    [SerializeField]
+    int storageCapacity;
+    int resouceStored = 0;
+    [SerializeField]
+    bool isStorageEnabled = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
