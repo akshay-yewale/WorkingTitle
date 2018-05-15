be89cd764fe8e8970f086eef1462f3ed202227c9
u1089828 <akshay.yewale@utah.edu>
2018-05-15 06:51:21 -0600

    Merge branch 'Suraj' into Akshay : by Akshay

--- Assets/Scripts/Controller/PlaneInputController.cs
+++ Assets/Scripts/Controller/PlaneInputController.cs
@@ -9,14 +9,23 @@ public class PlaneInputController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+        this.transform.position = new Vector3(1.0f, 2.25f, 10.0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
         float translationZ = Input.GetAxis("Horizontal") * speed * -1 * Time.deltaTime;
         float translationX = Input.GetAxis("Vertical") * speed * 10.0f * -1 * Time.deltaTime;
 
         transform.Translate((forwardspeed - translationX) * Time.deltaTime, 0, translationZ);
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Inside OnTriggerEnter");
+        if (other.gameObject.CompareTag("SingleBulletPrefab"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
