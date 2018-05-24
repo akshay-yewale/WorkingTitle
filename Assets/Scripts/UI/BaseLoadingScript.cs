69f6f1487c60ce402a33ceb2e16de738b21c7583
u1089828 <akshay.yewale@utah.edu>
2018-05-24 02:25:16 -0600

    UI system updated

--- Assets/Scripts/UI/BaseLoadingScript.cs
+++ Assets/Scripts/UI/BaseLoadingScript.cs
@@ -6,62 +6,49 @@ using UnityEngine.UI;
 
 public class BaseLoadingScript : MonoBehaviour
 {
-
-    private bool loadScene = false;
     public string LoadingSceneName;
-    int i;
+
+
+    [Header("DEBUG ONLY- REMOVE IN BUILD")]
+    [SerializeField]
+    private float f_WaitTime_Debug = 10.0f;
+    private bool loadScene = false;
 
     // Use this for initialization
-    void Start()
-    {
-        i = 0;
-
+    void Start(){
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        i++;
+    void Update(){
+     
         // If the player has pressed the space bar and a new scene is not loading yet...
         if (Input.GetKeyUp(KeyCode.Space) && loadScene == false)
         {
-
             // ...set the loadScene boolean to true to prevent loading a new scene more than once...
             loadScene = true;
 
             // ...and start a coroutine that will load the desired scene.
             StartCoroutine(LoadNewScene(LoadingSceneName));
-
-
-
-
+            
         }
         StartCoroutine(Waiting());
         //Invoke("WaitingMethod", 10f);
     }
+
     IEnumerator Waiting()
     {
-
-        yield return new WaitForSeconds(10f);
-        // Debug(i);
-
+        yield return new WaitForSeconds(f_WaitTime_Debug);
     }
     /*
     void WaitingMethod()
     {
         SceneManager.LoadScene(LoadingSceneName);
-
-
-
     }
     */
-
-
 
     // The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
     IEnumerator LoadNewScene(string sceneName)
     {
-
         // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
         AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
 
@@ -71,17 +58,7 @@ public class BaseLoadingScript : MonoBehaviour
             float progress = Mathf.Clamp01(async.progress / 10.0f);
 
             yield return null;
-
-
-
-
         }
         //yield return WaitForSeconds(10f);
-
-
     }
-
-
-
-
 }
---
+++ Assets/Scripts/UI/InterfaceManager_Base.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InterfaceManager_Base : MonoBehaviour {
+
+    // this is not used
+    /*void Update()
+    {
+        // Press the space key to start coroutine
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Use a coroutine to load the Scene in the background
+            StartCoroutine(LoadYourAsyncScene());
+        }
+    }*/
+
+    public void AttackButtonPressed(string name)
+    {
+        StartCoroutine(LoadYourAsyncScene(name));
+    }
+
+    public void BaseEditButtonPressed(string name)
+    {
+        StartCoroutine(LoadYourAsyncScene(name));
+    }
+
+
+    IEnumerator LoadYourAsyncScene(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
